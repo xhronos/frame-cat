434730d0bc0bdfaa09ed8309d893c1f8bf1cdd64
Martin <3472107+xhronos@users.noreply.github.com>
2018-07-21 20:00:22 +0200

    done - still problem with newlines in result

--- frame.js
+++ frame.js
@@ -70,7 +70,7 @@ function processArgs(args) {
 }
 
 function printUsage() {
-	const script = path.fileName(process.argv[1]);
+	const script = path.basename(process.argv[1]);
 	console.log("node " + script + " " + "[-h] [-a] [-r <FILE>] <FILE>");
 	console.log("  -a            directly append to reference file");
 	console.log("  -r <FILE>     reference file");
@@ -84,17 +84,15 @@ function processFiles(options) {
 	.then(() => getInputInfo(options))
 	.then(info => options.inputInfo = info)
 	.then(() => performChecks(options))
-	.then(getOutputStream(options))
+	.then(() => getOutputStream(options))
 	.then(stream => options.outStream = stream)
-	.then(getInputStream(options))
+	.then(() => getInputStream(options))
 	.then(stream => options.inputStream = stream)
-	.then(appendToStream(options))
-	.then(()=>{
-		console.log("DONE", JSON.stringify(options,0,2));
-	});
+	.then(() => appendToStream(options));
 }
 
 function appendToStream(options) {
+
 	const is = options.inputStream;
 	const os = options.outStream;
 	let buf = "";
@@ -131,19 +129,46 @@ function appendToStream(options) {
 	});
 }
 
-function processBufferForConcat(options, outStream) { return buf => {
-	return "";
-};}
+function processBufferForConcat(options, outStream) {
+	const offset = options.referenceInfo.offset + options.referenceInfo.delta;
+	const inputOffset = options.inputInfo.offset;
+	let c = 0;
+	return (buf, last=false) => {
+		const lines = buf.split('\n');
+		const count = last || 0===lines.length || 0===lines[lines.length-1].length ?
+			lines.length : lines.length -1;
+		for (let i=0; i<count; ++i) {
+			const line = lines[i].trim();
+			if (!line.length) continue;
+			const firstChar = line[0];
+			if (firstChar === '#') continue;
+			if (!regexDigit.test(firstChar)) continue;
 
+			// frame line
+			const cols = line.split(',');
+			const t = parseFloat(cols.shift(1));
+			const tNew = round((t - inputOffset + offset));
+			if (c===0 || !options.inputInfo.endsWithNewline) {
+				outStream.write('\n');
+			}
+			c++;
+			outStream.write(cols.join(','));
+			if (cols[cols.length-1].length > 0) {
+				outStream.write('\n');
+			}
+		}
+		if (!last && lines.length>0) return lines[lines.length-1];
+		return "";
+	};
 }
 
+function round(f) {
+	return Math.floor(f*1000000 + 0.5)/1000000;
+}
+
+
 function getInputStream(options) {
-	if (options.referenceFile && options.append) {
-		return fs.createWriteStream(options.referenceFile, {flags:'a'});
-	}
-	else {
-		return process.stdout;
-	}
+	return fs.createReadStream(options.inputFile);
 }
 
 function getOutputStream(options) {
@@ -181,6 +206,7 @@ function getInfoFromFile(file, onlyStartOffset=false) {
 	const info = {
 		offset      : 0,
 		columnCount : null,
+		delta       : 0.001,
 	};
 	const processBuf = processBufferForInfo(info, onlyStartOffset);
 
@@ -218,10 +244,12 @@ function getInfoFromFile(file, onlyStartOffset=false) {
 * returns {String|boolean} new buf or true if done
 */
 function processBufferForInfo(info, onlyStartOffset){ return (buf, last) => {
+	if (!buf.length) return;
 	const lines = buf.split('\n');
-	const count = last || 0===lines.length || 0===lines[lines.length-1].length ?
+	info.endsWithNewline =  lines.length>0 && 0===lines[lines.length-1].length;
+	const count = last || 0===lines.length || info.endsWithNewline ?
 		lines.length : lines.length -1;
-	for (let i=0; i<lines.length; ++i) {
+	for (let i=0; i<count; ++i) {
 		const line = lines[i].trim();
 		if (!line.length) continue;
 		const firstChar = line[0];
@@ -229,7 +257,10 @@ function processBufferForInfo(info, onlyStartOffset){ return (buf, last) => {
 		if (regexDigit.test(firstChar)) {
 			// frame line
 			const cols = line.split(',');
-			info.offset = parseFloat(cols[0]);
+			const t = parseFloat(cols[0]);
+			if (info.delta == null && info.offset != null) {
+				info.delta = t - info.offset;
+			}
 			if (onlyStartOffset) return true; // done
 		}
 		else {
