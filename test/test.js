3bfab9b09d60384c6821364c873514621fe03735
Martin <3472107+xhronos@users.noreply.github.com>
2018-07-22 18:15:20 +0200

    test fixes

--- test/test.js
+++ test/test.js
@@ -1,5 +1,13 @@
+var Promise = require('bluebird');
 const should = require('chai').should();
+const fs     = require('fs-extra');
 const mod    = require('../frame.js');
+
+const testDataDir = "./test/data/";
+const testFile_check     = testDataDir+'whole.txt';
+const testFile_reference = testDataDir+'1.txt';
+const testFile_output    = testDataDir+'out.txt';
+const testFile_input     = testDataDir+'2.txt';
 
 describe('frame', function(){
 	describe('cmdline args', function(){
@@ -55,31 +63,59 @@ describe('frame', function(){
 	});
 	describe('get info from file',function(){
 		it('should get reference file info (1)', function(){
-			return mod.getInfoFromFile('test/data/1.txt')
+			return mod.getInfoFromFile(testFile_reference)
 			.then(info=>{
 				info.offset.should.equal(20.916);
 				info.columnCount.should.equal(20);
 			});
 		});
 		it('should get reference file info (2)', function(){
-			return mod.getInfoFromFile('test/data/2.txt')
+			return mod.getInfoFromFile(testFile_input)
 			.then(info=>{
-				info.offset.should.equal(44.695);
+				info.offset.should.equal(24.695);
 				info.columnCount.should.equal(20);
 			});
 		});
 		it('should get input file info (1)', function(){
-			return mod.getInfoFromFile('test/data/1.txt', true)
+			return mod.getInfoFromFile(testFile_reference, true)
 			.then(info=>{
 				info.offset.should.equal(0);
 				info.columnCount.should.equal(20);
 			});
 		});
 		it('should get input file info (2)', function(){
-			return mod.getInfoFromFile('test/data/2.txt', true)
+			return mod.getInfoFromFile(testFile_input, true)
 			.then(info=>{
-				info.offset.should.equal(20.923);
+				info.offset.should.equal(0.923);
 				info.columnCount.should.equal(20);
+			});
+		});
+	});
+	describe('should concatenate test data',function(){
+		it('cleanup',function(){
+			return fs.unlink(testFile_output).catch(()=>{});
+		});
+		it('prepare',function(){
+			return fs.createReadStream(testFile_reference).pipe(fs.createWriteStream(testFile_output));
+		});
+		it('concatenate',function(){
+			const options = {
+				referenceFile : testFile_output,
+				inputFile     : testFile_input,
+				append        : true,
+			};
+			return mod.processFiles(options);
+		});
+		it('check concatenated file',function(){
+  			return Promise.all([
+				fs.readFile(testFile_output),
+				fs.readFile(testFile_check),
+			])
+			.spread((output, check)	=> {
+				output = "" + output;
+				check  = "" + check;
+				check.length.should.be.above(0);
+				output.should.equal(check);
 			});
 		});
 	});
